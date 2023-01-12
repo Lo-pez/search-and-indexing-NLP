8613fc38659644f43ea76c39772b890ebe84e5d9
Diego López <user@Diegos-MacBook-Pro-3.local>
2023-01-12 14:17:58 -0400

    first files

---
+++ indexer.py
@@ -0,0 +1,8 @@
+    """Given this dataset, write a program (in C or Python)
+    that creates an inverted index with the content of the 
+    Plot field. While tokenizing the dataset, apply stemming 
+    but keep the stopwords. [extra: keep a provision in the 
+    program to extend the inverted index with information from
+    other fields]
+    """
+    
---
+++ searcher.py
@@ -0,0 +1,10 @@
+    """
+    Write another program that uses the inverted index
+    produced by program 1 and performs a boolean search 
+    based on keyword queries given by the users during 
+    retrieval. [extra: incorporate a provision to make
+    a ranked retrieval that uses the term-frequency (tf)
+    of the query term in the document.
+    hit: store the term frequency of each word in the 
+    positioning list together with the doc_id.]
+    """
